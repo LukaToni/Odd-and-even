609b19862cae741e13451a0dc3965d6f51875ac1
Luka <luka.toni93@gmail.com>
2014-06-21 23:51:54 +0200

    Create OAE.c

---
+++ OAE.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    for(i = 1; i <= 10; i++)
+    {
+        if(i % 2 != 0)
+            printf("%d is odd\n", i );
+        else
+            printf("%d is even\n", i );
+    }
+    return 0;
+}
